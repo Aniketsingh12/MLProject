dd9016fa92b10a07417ba37ae594a168bc576022
Aniket Singh <67466295+Aniketsingh12@users.noreply.github.com>
2023-03-21 00:51:01 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphon_e_dot = '-e .'
+def get_Requirments(file_path:str)->List[str]:
+    '''this will return requirments.txt'''
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments = file_obj.readlines()
+        requirments = [req.replace("\n","") for req in requirments]
+        if hyphon_e_dot in requirments:
+            requirments.remove(hyphon_e_dot)
+    return requirments
+        
+setup(
+    name='Mlproject',
+    version='0.0.1',
+    author='Aniket',
+    author_email='surajsinghhh1342@gmail.com',
+    packages=find_packages(),
+    install_requires=get_Requirments('requirments.txt')
+    
+)
