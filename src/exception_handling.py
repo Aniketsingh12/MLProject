970402e313b258e623181ee5c1ca3b899e9a3658
Aniket Singh <67466295+Aniketsingh12@users.noreply.github.com>
2023-03-21 20:53:32 +0530

    src files

---
+++ src/exception_handling.py
@@ -0,0 +1,18 @@
+import sys
+
+def error_message_detail(error,error_detail:str):
+    _,_,error_tb = error_detail.exc_info()
+    file_name = error_tb.tb_frame.f_code.co_filename
+    error_message = "error on python script name [{0}] line number [{1}] error messgae[{3}] ".format(
+        file_name,error_tb.tb_lineno , str(error)       
+    )
+    return error_message
+
+class customException:
+    def __init__(self , error_message ,error_detail:sys ):
+        super.__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
